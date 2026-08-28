ddd651353bf98cbcaea141c48f84701a0f3207ba
Hirunthakan432 <hirunthakanhiru12@gmail.com>
2026-08-28 20:17:37 +0530

    Add largest of three project

---
+++ beginner/26_largest_of_three.pas
@@ -0,0 +1,9 @@
+program LargestOfThree;
+var a,b,c,largest: integer;
+begin
+  write('Enter three integers: '); readln(a,b,c);
+  largest := a;
+  if b > largest then largest := b;
+  if c > largest then largest := c;
+  writeln('Largest = ', largest);
+end.

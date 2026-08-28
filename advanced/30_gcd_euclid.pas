a005f109a7f8144767f8e8f787f22c295846414f
Hirunthakan432 <hirunthakanhiru12@gmail.com>
2026-08-28 20:19:14 +0530

    Fix Euclidean GCD implementation

--- advanced/30_gcd_euclid.pas
+++ advanced/30_gcd_euclid.pas
@@ -1,5 +1,20 @@
 program GCDEuclid;
-function GCD(a,b: longint): longint;
-begin while b<>0 do begin a:=a mod b; if a=0 then begin GCD:=b; exit; end; b:=b-a; end; GCD:=a; end;
-var a,b:longint;
-begin write('Enter two integers: '); readln(a,b); writeln('GCD = ',GCD(a,b)); end.
+
+function GCD(a, b: longint): longint;
+var t: longint;
+begin
+  while b <> 0 do
+  begin
+    t := a mod b;
+    a := b;
+    b := t;
+  end;
+  GCD := a;
+end;
+
+var a, b: longint;
+begin
+  write('Enter two integers: ');
+  readln(a, b);
+  writeln('GCD = ', GCD(a, b));
+end.
